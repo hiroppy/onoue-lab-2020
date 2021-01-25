1ae1d12937903a5fa1a7e4b4ff90e4ef45511191
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-01-25 19:52:00 +0900

    Update contents

--- slides/figure.js
+++ slides/figure.js
@@ -2,7 +2,14 @@ import React from "react";
 
 export const Figure = (props) => {
   return (
-    <figure style={{ maxWidth: props.width ?? "100%" }}>
+    <figure
+      style={{
+        maxWidth: props.width ?? "100%",
+        height: props.height ?? "auto",
+        // textAlign: props.position ?? "left",
+        margin: props.position === "center" ? "0 auto" : "0",
+      }}
+    >
       <img src={props.src} alt={props.alt} />
       {props.caption && <figcaption>{props.caption}</figcaption>}
     </figure>
