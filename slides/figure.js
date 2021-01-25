cfbf39d3b54711b7391489a3b19d9a6f57fb7506
arayaryoma <r1o.ryoma.abe@gmail.com>
2021-01-25 20:23:23 +0900

    Update contents

--- slides/figure.js
+++ slides/figure.js
@@ -7,7 +7,7 @@ export const Figure = (props) => {
         maxWidth: props.width ?? "100%",
         height: props.height ?? "auto",
         // textAlign: props.position ?? "left",
-        margin: props.position === "center" ? "0 auto" : "0",
+        margin: props.position === "center" ? "1em auto" : "1em 0",
       }}
     >
       <img src={props.src} alt={props.alt} />
